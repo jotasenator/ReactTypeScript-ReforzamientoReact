1bb93acceb43c929baedac5090cd543e4afc278f
Maykel <jotasenator@gmail.com>
2022-12-09 11:07:10 -0500

    usereducer dispatching increaseBy

--- src/bases/CounterExampleReducer.tsx
+++ src/bases/CounterExampleReducer.tsx
@@ -7,23 +7,61 @@ interface CounterState {
 }
 
 const INITIAL_STATE: CounterState = {
+  changes: 0,
   counter: 0,
   previous: 0,
-  changes: 0,
+};
+
+type CounterAction =
+  | { type: "increaseBy"; payload: { value: number } }
+  | { type: "reset" };
+
+const counterReducer = (
+  state: CounterState,
+  action: CounterAction
+): CounterState => {
+  const { counter, changes } = state;
+
+  switch (action.type) {
+    case "reset":
+      return {
+        changes: 0,
+        counter: 0,
+        previous: 0,
+      };
+    case "increaseBy":
+      return {
+        changes: changes + 1,
+        counter: counter + action.payload.value,
+        previous: counter,
+      };
+
+    default:
+      return state;
+  }
 };
 
 export const CounterUserReducer = () => {
-  const [state, dispatch] = useReducer(first, INITIAL_STATE);
+  const [counterState, dispatch] = useReducer(counterReducer, INITIAL_STATE);
 
-  const handleClick = () => {
-    setCounter((prev: number) => prev + 1);
+  const handleReset = () => {
+    dispatch({ type: "reset" });
+  };
+
+  const handleIncreaseBy = (value: number) => {
+    dispatch({ type: "increaseBy", payload: { value } });
   };
 
   return (
     <>
-      <h1>Counter : {counter} </h1>
+      <h1>Counter Reducer</h1>
 
-      <button onClick={handleClick}> +1 </button>
+      <pre>{JSON.stringify(counterState, null, 2)}</pre>
+
+      <button onClick={() => handleReset()}> Reset </button>
+      <button onClick={() => handleIncreaseBy(1)}> +1 </button>
+      <button onClick={() => handleIncreaseBy(5)}> +5 </button>
+      <button onClick={() => handleIncreaseBy(10)}> +10 </button>
     </>
   );
 };
